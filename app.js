73212ddae1319d765abe527d2dc8360079587f6a
root <root@linux.itu.com>
2026-02-24 16:55:25 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('DevOps Deployment From My CentOS VM 🚀');
+});
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('App running on port 3000');
+});
